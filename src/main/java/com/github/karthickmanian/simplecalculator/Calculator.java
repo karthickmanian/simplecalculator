e0c1fe600617bd34805a92af043850592431ad24
KARTHICKMANIAN S <maniankarthicks@gmail.com>
2019-01-17 13:03:38 +0530

    Add files via upload

---
+++ src/main/java/com/github/karthickmanian/simplecalculator/Calculator.java
@@ -0,0 +1,22 @@
+package com.github.karthickmanian.simplecalculator;
+
+/**
+ *  Simple program to understand test
+ */
+public class Calculator {
+    public static Integer add(Integer operator1, Integer operator2) {
+        return (operator1 + operator2);
+    }
+
+    public static Integer substract(Integer operator1, Integer operator2) {
+        return (operator1 - operator2);
+    }
+
+    public static Integer multiply(Integer operator1, Integer operator2) {
+        return (operator1 * operator2);
+    }
+
+    public static Integer divide(Integer operator1, Integer operator2) {
+        return (operator1 / operator2);
+    }
+}
